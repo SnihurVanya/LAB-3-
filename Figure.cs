3a355d0d18bad86d86b8420f4d3facd239896d27
SnihurVanya <I.Snihur_FIT_12_22_B_d@knute.edu.ua>
2023-10-02 22:50:36 +0300

    Lab3

---
+++ Figure.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Figure
+    {
+        private Point[] points;
+        private string cap;
+        private double perimeter;
+        public string cap { get { return title; } }
+        public double Perimeter
+        {
+            get
+            {
+                return perimeter;
+            }
+        }
+        public Figure(string cap, Point p1, Point p2, Point p3)
+        {
+            this.title = cap;
+            this.points = new Point[3];
+            this.points[0] = p1;
+            this.points[1] = p2;
+            this.points[2] = p3;
+            CalculatePerimeter();
+        }
+        public Figure(string cap, Point p1, Point p2, Point p3, Point p4)
+        {
+            this.cap = cap;
+            this.points = new Point[4];
+            this.points[0] = p1;
+            this.points[1] = p2;
+            this.points[2] = p3;
+            this.points[3] = p4;
+            CalculatePerimeter();
+        }
+        public Figure(string cap, Point p1, Point p2, Point p3, Point p4, Point p5)
+        {
+            this.cap = cap;
+            this.points = new Point[5];
+            this.points[0] = p1;
+            this.points[1] = p2;
+            this.points[2] = p3;
+            this.points[3] = p4;
+            this.points[4] = p5;
+            CalculatePerimeter();
+        }
+        private double GetSideLength(Point A, Point B)
+        {
+            double dx2 = (B.X - A.X) * (B.X - A.X);
+            double dy2 = (B.Y - A.Y) * (B.Y - A.Y);
+            double length = Math.Sqrt(dx2 + dy2);
+            return length;
+        }
+
+        private void CalculatePerimeter()
+        {
+            Point a, b;
+            double l;
+            this.perimeter = 0;
+            for (int i = 0; i < this.points.Length - 1; i++)
+            {
+                a = this.points[i];
+                b = this.points[i + 1];
+                l = GetSideLength(a, b);
+                this.perimeter += l;
+            }
+            a = this.points[0];
+            b = this.points[this.points.Length - 1];
+            l = GetSideLength(a, b);
+            this.perimeter += l;
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        static void Task1()
+
+        {
+            Console.WriteLine("Введіть першу довжину прямокутника:");
+            string conA = Console.ReadLine();
+            double A = Convert.ToDouble(conA);
+
+            Console.WriteLine("Введіть другу довжину прямокутника:");
+            string conB = Console.ReadLine();
+            double B = Convert.ToDouble(conB);
+
+            Rectangle nt = new Rectangle(A, B);
+            Console.WriteLine("Площа прямокутника: " + nt.Area);
+            Console.WriteLine("Периметер прямокутника: " + nt.CalculatePerimeter());
+
+            static void Task2()
+            {
+                Console.Write("Назва фігури: ");
+                string name = Console.ReadLine();
+                Console.Write("З скількох точок складається фігура (3-5): ");
+                string sNum = Console.ReadLine();
+                int num = Convert.ToInt32(sNum);
+                if (num < 3 || num > 5)
+                {
+                    Console.Write("Неправильне значення");
+                    return;
+                }
+                Point[] points = new Point[num];
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write("Точка" + i + " X=");
+                    string sX = Console.ReadLine();
+                    double x = Convert.ToDouble(sX);
+                    Console.Write("Точка" + i + " Y=");
+                    string sY = Console.ReadLine();
+                    double y = Convert.ToDouble(sY);
+                    points[i] = new Point(x, y);
+                }
+                Figure f;
+                if (num == 3)
+                    f = new Figure(name, points[0], points[1], points[2]);
+                else if (num == 4)
+                    f = new Figure(name, points[0], points[1], points[2], points[3]);
+                else
+                    f = new Figure(name, points[0], points[1], points[2], points[3], points[4]);
+
+                Console.WriteLine("Фігура " + f.cap + " має периметер " + f.Perimeter);
+            }
+            static void Main(string[] args)
+            }
+        }
+        
+    }
+}
+}
